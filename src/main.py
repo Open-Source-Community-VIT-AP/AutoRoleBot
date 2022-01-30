71e98af6f0c5a0ae1265a1cfcf970a90b4621ce0
Boomer3110 <92508110+Boomer3110@users.noreply.github.com>
2022-01-30 23:11:01 +0530

    Completed the script.

--- src/main.py
+++ src/main.py
@@ -1,40 +1,30 @@
 import discord
 from discord.ext import commands
-import csv
+import pandas as pd
 from dotenv import load_dotenv
+import csv
 import os
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-intents = discord.Intents().all()  # Allowing  the bot to access all the information
-bot = commands.Bot(command_prefix="==", intents=intents)
-
+intents = discord.Intents().all() # Allowing  the bot to access all the information
+bot = commands.Bot(command_prefix='==', intents=intents)
 
 @bot.event
-async def on_ready():  # Sends the bot is ready message in the console when it is done booting up
-    await bot.change_presence(
-        status=discord.Status.dnd,
-        activity=discord.Activity(
-            type=discord.ActivityType.watching, name="you sleep."
-        ),
-    )
+async def on_ready(): # Sends the bot is ready message in the console when it is done booting up 
+    await bot.change_presence(status=discord.Status.dnd ,activity=discord.Activity(type=discord.ActivityType.watching, name="you sleep."))
     print("Bot is ready!")
 
 
 def member_list(ctx):
-    memberlist = []  # Refreshes member list everytime command is run
-    member_dictionary = {}  # Member and member id key value pair
-    server_instance = ctx.message.guild.members  # Makes a server instance
+    memberlist =[] # Refreshes member list everytime command is run
+    member_dictionary = {} # Member and member id key value pair
+    server_instance = ctx.message.guild.members # Makes a server instance
     for member in server_instance:
-        memberlist.append(
-            member.name + "#" + member.discriminator
-        )  # Appends the discord tag in the desired format
-        member_dictionary[
-            member.name + "#" + member.discriminator
-        ] = member.id  # Makes the key value pair
-    return memberlist, member_dictionary
-
+        memberlist.append(member.name+"#"+member.discriminator) # Appends the discord tag in the desired format
+        member_dictionary[member.name+"#"+member.discriminator] = member.id # Makes the key value pair
+    return memberlist,member_dictionary
 
 def make_dictionary():
     csvFile = "data/participants.csv"
@@ -49,23 +39,37 @@ def make_dictionary():
     return dictionary
 
 
-@bot.command(name="roles", pass_context=True)
-@commands.has_role("Server Admin")  # Making sure only admin can run the command
-async def assign_all(ctx):
-    async with ctx.typing():  # Typing animation
-        memberlist, member_dictionary = member_list(ctx)  # Getting the the information
-        dictionary = make_dictionary()
-        for member in memberlist:
-            if member in dictionary:
-                user = ctx.author.guild.get_member(
-                    member_dictionary[member]
-                )  # Retrieves a discord.Member object
-                role = discord.utils.get(
-                    ctx.author.guild.roles, name=dictionary[member]
-                )  # Retrieves the required role
-                await user.add_roles(role)  # Assigns the role
-    await ctx.send("Done!")
 
+@bot.command(name="roles" , pass_context=True)
+@commands.has_role("Server Admin") # Making sure only admin can run the command
+async def assign_all(ctx):
+    async with ctx.typing(): # Typing animation
+        memberlist,member_dictionary = member_list(ctx) # Getting the the information
+        dictionary = make_dictionary()
+        global missing_list
+        missing_list = []
+        for member in dictionary:
+            if member in memberlist:
+                user = ctx.author.guild.get_member(member_dictionary[member]) # Retrieves a discord.Member object
+                role = discord.utils.get(ctx.author.guild.roles, name=dictionary[member]) # Retrieves the required role
+                await user.add_roles(role)  # Assigns the role
+            else:
+                temp =[]
+                temp.append(member)
+                missing_list.append(temp)
+
+
+@bot.command(name="left" , pass_context=True)
+@commands.has_role("Server Admin")
+async def missing(ctx):
+    fields = ["DISCORD ID OF MISSING PEOPLE",]
+    with open('Missing.csv', 'w') as f:
+        write = csv.writer(f)
+        write.writerow(fields)
+        for i in missing_list:
+            write.writerow(i)
+    await ctx.send(file=discord.File(r'Missing.csv'))
+    await ctx.send("Done!")
 
 if __name__ == "__main__":
     bot.run(TOKEN)
